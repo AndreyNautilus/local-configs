b00d8587ae5686030827a9f4ed44d730813500bd
Andrey Potapov <andrey.nautilus@yandex.ru>
2024-10-28 17:39:14 +0100

    fix(fish): use set-cursor with abbr

--- nix/fish/.config/fish/conf.d/git.fish
+++ nix/fish/.config/fish/conf.d/git.fish
@@ -8,16 +8,14 @@ abbr -a -g gss  "git status -s"
 abbr -a -g gp   "git pull"
 abbr -a -g gpo  "git push origin"
 
-abbr -a -g gl   "git log --topo-order --color --decorate --oneline -10 | awk '{print NR-1 \": \" \$0}'"
-abbr -a -g gl15 "git log --topo-order --color --decorate --oneline -15 | awk '{print NR-1 \": \" \$0}'"
+abbr -a -g --set-cursor=_CURSOR  gl   "git log --topo-order --color --decorate --oneline -10_CURSOR | awk '{print NR-1 \": \" \$0}'"
 abbr -a -g gl20 "git log --topo-order --color --decorate --oneline -20 | awk '{print NR-1 \": \" \$0}'"
-abbr -a -g gl30 "git log --topo-order --color --decorate --oneline -30 | awk '{print NR-1 \": \" \$0}'"
 
-abbr -a -g gcm  "git checkout main && git pull"
+abbr -a -g --set-cursor=_CURSOR gcm  "git checkout main_CURSOR && git pull"
 abbr -a -g gcms "git checkout master && git pull"
 
 abbr -a -g gs   "git switch"
-abbr -a -g gsm  "git switch main && git pull"
+abbr -a -g --set-cursor=_CURSOR gsm  "git switch main_CURSOR && git pull"
 abbr -a -g gsms "git switch master && git pull"
 
 abbr -a -g gf   "git commit --fixup=HEAD~"
