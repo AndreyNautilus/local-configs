7b8c57c19c8e98a21f653c67c0595fafc3c990ba
Andrey Potapov <andrey.nautilus@yandex.ru>
2025-08-12 17:36:05 +0200

    fix(fish): restore old ctrl-c behaviour (#3)
    According to [release notes of version
    4.0.0](https://fishshell.com/docs/current/relnotes.html#notable-backwards-incompatible-changes):
    > ctrl-c now calls a new bind function called clear-commandline. The old
    behavior, which leaves a “^C” marker, is available as cancel-commandline
    
    So, restore the old behaviour.

--- nix/fish/.config/fish/conf.d/common/config.fish
+++ nix/fish/.config/fish/conf.d/common/config.fish
@@ -2,3 +2,7 @@ set -g fish_color_command brwhite
 set -g fish_prompt_pwd_dir_length 0
 
 set -g color_path_basename white
+
+# For fish >= 4 bring back the old ctrl-c behaviour:
+#   leave the current cancelled command, print the cancel mark and print the new prompt.
+bind ctrl-c cancel-commandline
