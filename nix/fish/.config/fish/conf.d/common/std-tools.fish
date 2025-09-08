cdf56f37a2650f762d12363328f19108f279935e
Andrey Potapov <andrey.potapov@tomtom.com>
2025-09-08 18:17:27 +0200

    fix(fish): add more std-tools abbreviations

--- nix/fish/.config/fish/conf.d/common/std-tools.fish
+++ nix/fish/.config/fish/conf.d/common/std-tools.fish
@@ -1,7 +1,12 @@
 abbr -a -g  tar_c_gz    tar -czvf out.tar.gz .
-abbr -a -g --set-cursor=_C tar_un_gz    tar -xzvf file_C.tar.gz
-abbr -a -g --set-cursor=_C tar_un_bz2   tar -xjvf file_C.tar.bz2
+abbr -a -g --set-cursor=_C  tar_un_gz   tar -xzvf file_C.tar.gz
+abbr -a -g --set-cursor=_C  tar_un_bz2  tar -xjvf file_C.tar.bz2
+abbr -a -g --set-cursor=_C  tar_list    tar -tvf file_C.tar.gz
 
 
 abbr -a -g  gpg_encr    gpg --symmetric --cipher-algo AES-256
 abbr -a -g  gpg_decr    gpg --decrypt
+
+
+abbr -a -g  find_exec   'find . -name "*.txt" -exec echo == {} \;'
+abbr -a -g  find_exec_grep  'find . -name "*.txt" -exec sh -c "echo {} && grep PATTERN {}" \;'
