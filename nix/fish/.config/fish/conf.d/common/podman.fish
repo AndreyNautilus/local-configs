db75b68233725063adb1d643d9d0a946b6f39c1f
Andrey Potapov <andrey.nautilus@yandex.ru>
2025-09-11 20:18:01 +0200

    fix(fish): define docker alias for podman conditionally (#12)
    Co-authored-by: Andrey Potapov <andrey.potapov@tomtom.com>

--- nix/fish/.config/fish/conf.d/common/podman.fish
+++ nix/fish/.config/fish/conf.d/common/podman.fish
@@ -1,1 +1,6 @@
-alias docker=podman
+if not type -q docker and type -q podman
+    # define alias ONLY if:
+    # - podman is a valid command
+    # - docker is NOT a valid command
+    alias docker=podman
+end
