8db19aabf761a4b4e4b281bd6da365ae616ff0a7
Andrey Potapov <andrey.nautilus@yandex.ru>
2025-09-20 23:57:40 +0200

    fix(win): bash-like completion in windows terminal (#15)

--- win/windows-terminal/Microsoft.PowerShell_profile.ps1
+++ win/windows-terminal/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,10 @@
 Import-Module posh-git
 
 oh-my-posh init pwsh --config "$HOME\Documents\PowerShell\andrey-nautilus.omp.json" | Invoke-Expression
+
+# enable bash-like TAB-completion
+# arrow up/down search in history for completion
+# See: https://stackoverflow.com/questions/8264655/how-to-make-powershell-tab-completion-work-like-bash
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
