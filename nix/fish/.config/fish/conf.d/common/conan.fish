392f9b0769a37b8b273657fdad1e1f188f1f051a
Andrey Potapov <andrey.potapov@tomtom.com>
2025-11-10 18:46:37 +0100

    fix(fish): update abbreviations for conan2

--- nix/fish/.config/fish/conf.d/common/conan.fish
+++ nix/fish/.config/fish/conf.d/common/conan.fish
@@ -1,8 +1,8 @@
-abbr -a -g 	coni	"conan install . -pr=macos_armv8 --build=missing"
-abbr -a -g 	conb	"conan build ."
-abbr -a -g  conc    "conan create . (git describe --tags)@andrey/testing -pr=macos_armv8 --build=missing"
+abbr -a -g 	coni	"conan install . -pr:h=macos_armv8 --build=missing"
+abbr -a -g 	conb	"conan build . -pr:h=macos_armv8"
+abbr -a -g  conc    "conan create . --version=(git describe --tags) --user=andrey --channel=testing -pr:h=macos_armv8 --build=missing"
 abbr -a -g  cons    'conan search "*"'
 abbr -a -g  conr    "conan remove -f"
 
 abbr -a -g  con_clean   'conan remove --builds --src --force "*"'
-abbr -a -g  con_default 'rm ~/.conan/profiles/default'
+abbr -a -g  con_re_default 'rm ~/.conan2/profiles/default && conan profile detect'
